975db3bdd22ab131338c9fa9974191888595637e
sonickun <yumehisa.0407@gmail.com>
2015-11-05 14:47:12 +0900

    second

---
+++ Chapter2/tcp_server.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AP_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server.listen(5)
+
+print "[*] Listening on %s:%d" %  (bind_ip, bind_port)
+
+# thread which process connection from client
+def handle_client(client_socket):
+	
+	# Display the data client sent
+	request = client_socket.recv(1024)
+	print "[*] Recieved: %s" % request
+
+	# Return packet
+	client_socket.send("ACK!")
+	client_socket.close()
+
+while True:
+	client, addr = server.accept()
+
+	print "[*] Accepted connection from: %s:%d" % (add[0],addr[1])
+
+	# Run thread processing recieved data
+	client_handler = threading.Thread(target=handle_client, args=(client,))
+	client_handler.start()
+
+
